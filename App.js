07ea3f7bf91a738de6a3186b0969e43aa0170728
ldm0408 <ldm0408@gmail.com>
2018-08-22 18:54:46 +0900

    finish

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Timer from "./components/Timer";
+import reducer from "./reducer";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+let store = createStore(reducer); // reducer.js 의 reducer함수를 호출, reducer함수의 default는 initialState이다
 
 export default class App extends React.Component {
   render() {
-    return <Timer />;
+    return (
+      <Provider store={store}>
+        <Timer />
+      </Provider> // Provider 가 자동으로 Timer componet에 reducer의 initailState를 복사해서 넘겨줌
+    );
   }
 }
 
---
+++ components/Timer/presenter.js
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
+import Button from "../Button";
+
+function formatTime(time) {
+  var minutes = Math.floor(time / 60);
+  time -= minutes * 60;
+
+  var seconds = parseInt(time % 60, 10);
+
+  return `${minutes < 10 ? `0${minutes}` : minutes}:${
+    seconds < 10 ? `0${seconds}` : seconds
+  }`;
+}
+
+class Timer extends Component {
+  componentWillReceiveProps(nextProps) {
+    const currentProps = this.props;
+    if (!currentProps.isPlaying && nextProps.isPlaying) {
+      const timerInterval = setInterval(() => {
+        currentProps.addSecond();
+      }, 1000);
+      this.setState({
+        interval: timerInterval
+      });
+    } else if (currentProps.isPlaying && !nextProps.isPlaying) {
+      clearInterval(this.state.interval);
+    }
+  }
+  render() {
+    console.log(this.props);
+    const {
+      isPlaying,
+      elapsedTime,
+      timerDuration,
+      startTimer,
+      pauseTimer,
+      addSecond
+    } = this.props;
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle={"light-content"} />
+        <View style={styles.upper}>
+          <Text style={styles.timer}>
+            {formatTime(timerDuration - elapsedTime)}
+          </Text>
+        </View>
+        <View style={styles.lower}>
+          {isPlaying ? (
+            <Button iconName="stop-circle" onPress={pauseTimer} />
+          ) : (
+            <Button iconName="play-circle" onPress={startTimer} />
+          )}
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#CE0B24"
+  },
+  upper: {
+    flex: 2,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  lower: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  timer: {
+    color: "white",
+    fontSize: 120,
+    fontWeight: "100"
+  }
+});
+
+export default Timer;
