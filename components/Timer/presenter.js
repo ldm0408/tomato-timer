43fb8e73cacb6901f69bfa559291ca4193089900
ldm0408 <ldm0408@gmail.com>
2018-08-27 17:45:18 +0900

    modified

--- components/Timer/presenter.js
+++ components/Timer/presenter.js
@@ -6,7 +6,7 @@ function formatTime(time) {
   var minutes = Math.floor(time / 60);
   time -= minutes * 60;
 
-  var seconds = parseInt(time % 60, 10);
+  var seconds = time;
 
   return `${minutes < 10 ? `0${minutes}` : minutes}:${
     seconds < 10 ? `0${seconds}` : seconds
